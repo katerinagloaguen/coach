89e9cd796a7dd2b326d2d5ff66de578bf3fa9fb2
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2023-06-11 09:34:48 +0200

    scrollify support

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,32 @@
-$(document).ready(function() {
+jQuery(function() {
+    // scrollify
+    $.scrollify({
+      section: ".panel",
+      sectionName: false,
+      interstitialSection: ".header,.footer",
+      before:function(i,panels) {
+        let ref = panels[i].attr("data-section-name");
+        $(".pagination .active").removeClass("active");
+        $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+      },
+      afterRender:function() {
+        let pagination = "<ul class=\"pagination\">";
+        let activeClass = "";
+        $(".panel").each(function(i) {
+          activeClass = "";
+          if (i===$.scrollify.currentIndex()) {
+            activeClass = "active";
+          }
+          pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+        });
+
+        pagination += "</ul>";
+
+        $(".home").append(pagination);
+        $(".pagination a").on("click",$.scrollify.move);
+      }
+    });
+
     // Event handler for the theme switch
     $('#themeToggle').click(function() {
       $('body').toggleClass('dark-theme');
