69f9957904071e020311f406cc1a0ecd39dceddd
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2023-06-14 10:59:22 +0200

    better lang dictionnary

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
 jQuery(function() {
     console.log('If you are seeing this, the errors you are seeing come from YouTube');
 
+    let availableLangs = ['en', 'fr'];
+    let dictionary = [];
+    let currentLang = 'en';
+
+    for (const lang of availableLangs) {
+      $.getJSON('./lang/' + lang + '.json', function(data) {
+        dictionary[lang] = data;
+      }).fail(function(){
+        console.log('Could not load lang file ' + lang);
+      });
+    }
+
     // scrollify
     $.scrollify({
       section: ".panel",
@@ -26,36 +38,37 @@ jQuery(function() {
     $('#themeToggle').on('click', function() {
       $('body').toggleClass('dark-theme');
       let themeIcon = $('#themeToggle span.fa');
+      let themeText = $('#themeToggle span.hover-text');
+      currentLang = $('#langToggle').data('lang');
       if ($('body').hasClass('dark-theme')) {
         themeIcon.removeClass('fa-moon');
         themeIcon.addClass('fa-sun');
+        themeText.text(dictionary[currentLang]['navbar_theme_light']);
       } else {
         themeIcon.removeClass('fa-sun');
         themeIcon.addClass('fa-moon');
+        themeText.text(dictionary[currentLang]['navbar_theme_dark']);
       }
     });
 
     // Event handler for the lang switch
     $('#langToggle').on('click', function() {
       let $langBtn = $(this);
-      let currentLang = $langBtn.data('lang');
+      currentLang = $langBtn.data('lang');
       let newLang = (currentLang === 'en' ? 'fr' : 'en');
-      $.getJSON('./lang/' + newLang + '.json', function(data) {
-        $('[lang-tag]').each(function () {
-          let $element = $(this);
-          let langTag = $element.attr('lang-tag');
-          $element.html(data[langTag]);
-        });
-        $langBtn.data('lang', newLang);
-        $('html').attr('lang', newLang);
-        if (newLang === 'en') {
-          $langBtn.find('img').attr('src', './images/france.png');
-        } else {
-          $langBtn.find('img').attr('src', './images/uk.jpg');
-        }
-      }).fail(function(){
-        console.log('Could not load lang file');
+
+      $('[lang-tag]').each(function () {
+        let $element = $(this);
+        let langTag = $element.attr('lang-tag');
+        $element.html(dictionary[newLang][langTag]);
       });
+      $langBtn.data('lang', newLang);
+      $('html').attr('lang', newLang);
+      if (newLang === 'en') {
+        $langBtn.find('img').attr('src', './images/france.png');
+      } else {
+        $langBtn.find('img').attr('src', './images/uk.jpg');
+      }
     });
 
     $('#copyright').text('©' + new Date().getFullYear());
